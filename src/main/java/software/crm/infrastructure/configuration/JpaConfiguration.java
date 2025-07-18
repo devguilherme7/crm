0fae3e7c0df9a4ed2949249cb61a8b8b3c074145
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-07-17 21:16:20 -0300

    Add JPA configuration

---
+++ src/main/java/software/crm/infrastructure/configuration/JpaConfiguration.java
@@ -0,0 +1,12 @@
+package software.crm.infrastructure.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+public class JpaConfiguration {
+
+}
